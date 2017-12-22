66fe6bf519af208e8f77b1d36f9aac91aef52f36
Irene Arsuaga <irenearsu@gmail.com>
2017-12-22 16:43:59 +0100

    Proyecto con las pantallas implementadas

---
+++ ADB/app/src/main/java/eus/ehu/adibidea/tta/adb/Presentacion/LoginActivity.java
@@ -0,0 +1,16 @@
+package eus.ehu.adibidea.tta.adb.Presentacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import eus.ehu.adibidea.tta.adb.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio);
+    }
+}
+
