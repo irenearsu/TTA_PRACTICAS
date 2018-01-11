fe2a40e8fddb62d6cd44cc9af052134a6bf509c5
Irene Arsuaga <irenearsu@gmail.com>
2018-01-11 23:23:43 +0100

    Grabacion de audio y intents de camara y video implementados

--- ADB/app/src/main/java/eus/ehu/adibidea/tta/adb/Presentacion/EjercicioActivity.java
+++ ADB/app/src/main/java/eus/ehu/adibidea/tta/adb/Presentacion/EjercicioActivity.java
@@ -1,15 +1,110 @@
 package eus.ehu.adibidea.tta.adb.Presentacion;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 import eus.ehu.adibidea.tta.adb.R;
 
 public class EjercicioActivity extends AppCompatActivity {
 
+    Uri pictureUri;
+
+    public static final int PICTURE_REQUEST_CODE = 0;
+    public static final int VIDEO_REQUEST_CODE = 1;
+    public static final int AUDIO_REQUEST_CODE = 2;
+    public static final int READ_REQUEST_CODE = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio);
+    }
+
+    protected void fichero(View biew){
+
+
+
+    }
+
+    protected void foto (View view){
+
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
+            Toast.makeText(this,R.string.no_camera,Toast.LENGTH_SHORT).show();
+        else{
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(intent.resolveActivity(getPackageManager())!=null){
+                File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                try{
+                    File file = File.createTempFile("tta",".jpg",dir);
+                    pictureUri = Uri.fromFile(file);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT,pictureUri);
+                    startActivityForResult(intent,PICTURE_REQUEST_CODE);
+                }
+                catch(IOException e){
+
+                }
+            }
+            else
+                Toast.makeText(this,R.string.no_camera,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void rec_video(View view){
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
+            Toast.makeText(this,R.string.no_camera,Toast.LENGTH_SHORT).show();
+        else{
+            Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+            if(intent.resolveActivity(getPackageManager())!=null)
+                startActivityForResult(intent,VIDEO_REQUEST_CODE);
+            else
+                Toast.makeText(this,R.string.no_camera,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected  void rec_audio(View view){
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE))
+            Toast.makeText(this,R.string.no_micro,Toast.LENGTH_SHORT).show();
+        else{
+            Intent intent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
+            if (intent.resolveActivity(getPackageManager()) != null)
+                startActivityForResult(intent,AUDIO_REQUEST_CODE);
+            else
+                Toast.makeText(this,R.string.no_micro,Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    public void sendFile(Uri uri){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        startActivityForResult(intent,READ_REQUEST_CODE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode != Activity.RESULT_OK)
+            return;
+        switch (requestCode){
+            case READ_REQUEST_CODE:
+            case VIDEO_REQUEST_CODE:
+            case AUDIO_REQUEST_CODE:
+                sendFile(data.getData());
+                break;
+            case PICTURE_REQUEST_CODE:
+                sendFile(pictureUri);
+                break;
+        }
     }
 }
